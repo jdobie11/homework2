6975c8a0004559a9ff8fe2783eb103fa19c37144
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-02-09 20:32:11 +0000

    movies added

--- kmdb.rb
+++ kmdb.rb
@@ -85,9 +85,41 @@ Role.destroy_all
 # Generate models and tables, according to the domain model.
 # TODO!
 
+#studio
+warner_bros = Studio.new
+warner_bros.name = "Warner Bros."
+warner_bros.save
+
+#movie
+batman_begins = Movie.new
+batman_begins.title = "Batman Begins"
+batman_begins.year_released = 2005
+batman_begins.rated = "PG-13"
+batman_begins.studio = warner_bros # Use the association
+batman_begins.save
+
+dark_knight = Movie.new
+dark_knight.title = "The Dark Knight"
+dark_knight.year_released = 2008
+dark_knight.rated = "PG-13"
+dark_knight.studio = warner_bros # Use the association
+dark_knight.save
+
+dark_knight_rises = Movie.new
+dark_knight_rises.title = "The Dark Knight Rises"
+dark_knight_rises.year_released = 2012
+dark_knight_rises.rated = "PG-13"
+dark_knight_rises.studio = warner_bros # Use the association
+dark_knight_rises.save
+
+
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
+
+
+
+
 
 # Prints a header for the movies output
 puts "Movies"
